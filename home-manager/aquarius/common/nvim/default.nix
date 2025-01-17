cc5d24b91eed83eb1960e51d1f5830d1ad723cbc
AJ <camellia.snow.flower@gmail.com>
2025-01-17 15:14:11 -0600

    fucking around with nvf

--- home-manager/aquarius/common/nvim/default.nix
+++ home-manager/aquarius/common/nvim/default.nix
@@ -13,19 +13,28 @@
     vimAlias = true;
     theme = {
      enable = true; 
-     name = "catppucin";
+     name = "gruvbox";
      style = "dark";
     };
    
     statusline.lualine.enable = true;
     telescope.enable = true;
     autocomplete.nvim-cmp.enable = true;
+    filetree.neo-tree.enable = true;
+    tabline.nvimBufferline.enable = true;
+    
+    binds = {
+     whichKey.enable = true;
+     cheatsheet.enable = true;
+    };
 
     languages = {
      enableLSP = true;
      enableTreesitter = true;
+     enableFormat = true;
 
      nix.enable = true;
+     markdown.enable = true;
     };
    };
   };
