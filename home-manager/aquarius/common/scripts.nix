0a74547fba646f99b1ce5afbd98917a768813d8a
AJ <camellia.snow.flower@gmail.com>
2025-01-21 17:21:32 -0600

    updated scripts

--- home-manager/aquarius/common/scripts.nix
+++ home-manager/aquarius/common/scripts.nix
@@ -6,8 +6,8 @@
 }: {
   # Home-manager Script
   home.packages = with pkgs; [
-    (writeShellScriptBin "hms" ''home-manager switch --flake .#aquarius@saturn |& nom'')
-    (writeShellScriptBin "hmsb" ''home-manager switch -b backup --flake .#aquarius@saturn |& nom'')
+    (writeShellScriptBin "hms" ''home-manager switch --flake .#$USER@$HOSTNAME |& nom'')
+    (writeShellScriptBin "hmsb" ''home-manager switch -b backup --flake .#$USER@$HOSTNAME |& nom'')
   ];
   #TODO need to make more scripts for automating git and/or look into lazy git
 }
