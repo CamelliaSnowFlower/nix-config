9ad12aef9fc55952b35e9e95d40f785f3fb1bb30
CamelliaSnowFlower <camelliasnowflower@gmail.com>
2025-01-22 19:39:32 -0600

    hh added

--- home-manager/aquarius/common/core/scripts.nix
+++ home-manager/aquarius/common/core/scripts.nix
@@ -8,7 +8,7 @@
   home.packages = with pkgs; [
     (writeShellScriptBin "hms" ''home-manager switch --flake .#$USER@$HOSTNAME |& nom'')
     (writeShellScriptBin "hmsb" ''home-manager switch -b backup --flake .#$USER@$HOSTNAME |& nom'')
-    (writeShellScriptBin "home" ''cd /home/$USER/Documents/nix-config/'')
+    (writeShellScriptBin "hh" ''cd /home/$USER/Documents/nix-config/'')
   ];
   #TODO need to make more scripts for automating git and/or look into lazy git
 }
