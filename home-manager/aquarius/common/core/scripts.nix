160676aff06ad66373f0779f48b4dc1884f2edf8
CamelliaSnowFlower <164219867+CamelliaSnowFlower@users.noreply.github.com>
2025-01-22 19:50:22 -0600

    Update scripts.nix

--- home-manager/aquarius/common/core/scripts.nix
+++ home-manager/aquarius/common/core/scripts.nix
@@ -10,5 +10,4 @@
     (writeShellScriptBin "hmsb" ''home-manager switch -b backup --flake .#$USER@$HOSTNAME |& nom'')
     (writeShellScriptBin "hh" ''echo /home/$USER/Documents/nix-config/'')
   ];
-  #TODO need to make more scripts for automating git and/or look into lazy git
 }
