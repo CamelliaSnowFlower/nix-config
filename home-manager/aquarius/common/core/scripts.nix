039259838564d138cf2014972aa8c739f7a375be
AJ <camellia.snow.flower@gmail.com>
2025-02-01 23:30:37 -0600

    vim .

--- home-manager/aquarius/common/core/scripts.nix
+++ home-manager/aquarius/common/core/scripts.nix
@@ -8,6 +8,6 @@
   home.packages = with pkgs; [
     (writeShellScriptBin "hms" ''home-manager switch --flake .#$USER@$HOSTNAME |& nom'')
     (writeShellScriptBin "hmsb" ''home-manager switch -b backup --flake .#$USER@$HOSTNAME |& nom'')
-    (writeShellScriptBin "hh" ''cd ~/Documents/nix-config/ && $SHELL'')
+    (writeShellScriptBin "hh" ''cd ~/Documents/nix-config/ && $SHELL && vim .'')
   ];
 }
