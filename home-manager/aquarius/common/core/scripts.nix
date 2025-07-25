20ce49f8e1301bcfe74d07a2a6467430d000c7fc
CamelliaSnowFlower <camelliasnowflower@gmail.com>
2025-07-25 05:23:16 -0500

    I think we chilling on the pokemonsay spotify commands

--- home-manager/aquarius/common/core/scripts.nix
+++ home-manager/aquarius/common/core/scripts.nix
@@ -10,8 +10,8 @@
     (writeShellScriptBin "hmsb" ''home-manager switch -b backup --flake .#$USER@$HOSTNAME |& nom'')
     (writeShellScriptBin "hh" ''cd /home/aquarius/Documents/nix-config/'')
     (writeShellScriptBin "skip" ''echo "Up Next!" && spotifycli --next && spotifycli --song && echo "by" && spotifycli --artist'')
-   (writeShellScriptBin "rewind" ''echo "Run it Back" && spotifycli --prev && spotifycli --song && echo "by" && spotifycli --artist'')
-   (writeShellScriptBin "rwp" ''rewind | pokemonsay -N'')
-   (writeShellScriptBin "skp" ''skip | pokemonsay -N'')
+   (writeShellScriptBin "rewind" ''echo "Run it Back:" && spotifycli --prev && spotifycli --song && echo "by" && spotifycli --artist'')
+   (writeShellScriptBin "rwp" ''rewind | pokemonsay'')
+   (writeShellScriptBin "skp" ''skip | pokemonsay'')
   ];
 }
