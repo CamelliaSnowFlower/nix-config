8c6156b534836c50724324ee17c1301059b9fda5
CamelliaSnowFlower <camelliasnowflower@gmail.com>
2025-07-25 05:30:59 -0500

    playpause added

--- home-manager/aquarius/common/core/scripts.nix
+++ home-manager/aquarius/common/core/scripts.nix
@@ -9,9 +9,12 @@
     (writeShellScriptBin "hms" ''home-manager switch --flake .#$USER@$HOSTNAME |& nom'')
     (writeShellScriptBin "hmsb" ''home-manager switch -b backup --flake .#$USER@$HOSTNAME |& nom'')
     (writeShellScriptBin "hh" ''cd /home/aquarius/Documents/nix-config/'')
+    #spotifycli commands with pokemonsay intergration
     (writeShellScriptBin "skip" ''echo "Up Next!" && spotifycli --next && spotifycli --song && echo "by" && spotifycli --artist'')
    (writeShellScriptBin "rewind" ''echo "Run it Back:" && spotifycli --prev && spotifycli --song && echo "by" && spotifycli --artist'')
+   (writeShellScriptBin "playpause" ''echo ">||" && spotifycli --playpause && spotifycli --song && echo "by" && spotifycli --artist'')
    (writeShellScriptBin "rwp" ''rewind | pokemonsay'')
    (writeShellScriptBin "skp" ''skip | pokemonsay'')
+   (writeShellScriptBin "pp" ''playpause | pokemonsay'')
   ];
 }
