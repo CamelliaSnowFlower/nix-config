3dcfb632c38220b3c991103821090c6763d984ab
CamelliaSnowFlower <camelliasnowflower@gmail.com>
2026-03-06 08:58:57 -0600

    turned on Intel GPU in bios and found bus Id

--- hosts/uranus/nvidia.nix
+++ hosts/uranus/nvidia.nix
@@ -10,8 +10,17 @@
       powerManagement.enable = false;
       powerManagement.finegrained = false;
       open = false;
-      package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
     
+      prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true;
+
+      # run the following to find the pci ports for the graphics cards
+      # nix shell nixpkgs#pciutils -c lspci | grep VGA
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+      };
     };
   };
   services.xserver.videoDrivers = ["nvidia"];
