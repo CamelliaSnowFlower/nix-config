ccc226f989c27f5796c25530bb4fec84c7a1f2e7
CamelliaSnowFlower <camelliasnowflower@gmail.com>
2026-03-24 16:54:47 -0500

    We scripting out here

---
+++ hosts/uranus/scripts.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  # Nix Scripts
+  environment.packages = with pkgs; [
+    (writeShellScriptBin "Duo" ''gnome-monitor-config set -LM HDMI-1 -m 1920x1080@75.002 -t normal -LpM DP-3 -y 1080 -t normal -m 2560x1440@164.956'')
+    (writeShellScriptBin "Gamer" ''gnome-monitor-config set -LpM DP-3 -t normal -m 2560x1440@164.956'')
+    (writeShellScriptBin "TV" ''gnome-monitor-config set -LpM HDMI-1 -m 1920x1080@75.002 -t normal'')
+     ];
+}
