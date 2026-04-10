8990d7a202b3e22b2ca87ba06c6ec557c50effe1
CamelliaSnowFlower <camelliasnowflower@gmail.com>
2026-04-10 11:31:49 -0500

    font settings basic

--- home-manager/aquarius/common/core/stylix/default.nix
+++ home-manager/aquarius/common/core/stylix/default.nix
@@ -14,6 +14,26 @@
     enable = true;
     image = ./TADC.jpg;
     polarity ="dark";
-    targets.librewolf.profileNames = ["aquarius"];
+    fonts = {
+      serif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+      };
+
+      monospace = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans Mono";
+      };
+      
+      emoji = {
+      package = pkgs.noto-fonts-color-emoji;
+      name = "Noto Color Emoji";
+      };
+    };
 };
 }
